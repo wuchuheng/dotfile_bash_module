95a37a59e21d1c09b5b8109888b2bc8846ea9623
wuchuheng <root@wuchuheng.com>
2023-11-06 19:50:27 +0800

    feat: throw the exception with callstack

--- src/handlers/exception_handler.sh
+++ src/handlers/exception_handler.sh
@@ -11,10 +11,25 @@ import @/src/utils/color_printf.sh
 ##
 function throw() {
   local test_file=${BASH_SOURCE[1]}
-  current_file=${test_file:${prefix_path_len}}
+  local current_file=${test_file:${prefix_path_len}}
   local line="${BASH_LINENO[0]}"
-  printf "$(bg_red_print " EXCEPTION ") ${current_file}:${line} $(red_print "$1")\n"
+  printf "$(bg_red_print " EXCEPTION ") $(red_print "$1")\n"
+  # print the part of codecc
   get_a_part_of_code ${current_file} ${line}
+  # print the call stack
+  local frame=1
+  local frame_count=${#FUNCNAME[@]}
+  while [ "${frame}" -lt "${frame_count}" ]; do
+    local func="${FUNCNAME[${frame}]}"
+    local stackLine="${BASH_LINENO[$((frame - 1))]}"
+    local src="${BASH_SOURCE[${frame}]}"
+    local src_file=${src:${prefix_path_len}}
+    if [[ "${src_file:0:${#DOTFILES_BASE_PATH}}" == "${DOTFILES_BASE_PATH}"  ]]; then
+      src_file=${src_file:${#DOTFILES_BASE_PATH} + 1}
+    fi
+    printf "    ${src_file}:${stackLine} ${func}\n"
+    ((frame++))
+  done
   exit 1
 }
 
