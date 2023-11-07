e8c41289290e67df137afcc1c53ca0c093dc5d02
wuchuheng <root@wuchuheng.com>
2023-11-07 13:00:17 +0800

    refactory: make the code more readability

--- src/config/common_conf.sh
+++ src/config/common_conf.sh
@@ -11,10 +11,7 @@ FALSE=1
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   OS=$(cat /etc/os-release | grep NAME= | awk 'NR == 1 {print $0}' |  sed -n 's/NAME="\([^"]*\)".*/\1/p')
   case ${OS} in
-    'Ubuntu')
-        GLOBAL_OS='Ubuntu';
-    ;;
-    PRETTY_Ubuntu*)
+    'Ubuntu'|PRETTY_Ubuntu*)
         GLOBAL_OS='Ubuntu';
     ;;
     'CentOS Linux')
